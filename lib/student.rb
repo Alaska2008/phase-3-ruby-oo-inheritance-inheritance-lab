ebf5a7bc1944c9b329a78fc37a3c64c76c2bff0c
Amen Atikpo <alaskale2024@gmail.com>
2022-08-27 14:13:41 +0000

    Done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+class Student < User
+    def initialize
+        @knowledge = []
+    end
+    def learn(word)
+        @knowledge << word
+    end
+    def knowledge
+        @knowledge
+    end
 
 end
